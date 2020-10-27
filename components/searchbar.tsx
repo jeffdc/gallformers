2732ada6e3cbc2183afa222c0ebdff0d8493dc50
jeffdc <jeff@nothoo.com>
2020-10-26 20:47:59 -0400

    Added global search hack page

--- components/searchbar.tsx
+++ components/searchbar.tsx
@@ -32,7 +32,7 @@ export const SearchBar = ( {query}: Props): JSX.Element => {
     return (
         <Container fluid className='m-1'>
             <Row className='small border border-secondary bg-dark rounded-sm fixed-bottom text-light text-center mt-5'>
-                <Col className="border align-items-center d-flex">Host: <i>{query.host}</i></Col>
+                <Col className="border align-items-center d-flex">Host: <i>{anyIfEmptyString(query.host)}</i></Col>
                 <Col className="border align-items-center d-flex">Detachable: {anyIfEmptyString(query.detachable)}</Col>
                 <Col className="border align-items-center d-flex">Location(s): {arrayToString(query.locations)}</Col>
                 <Col className="border align-items-center d-flex">Alignment: {anyIfEmptyString(query.alignment)}</Col>
---
+++ pages/globalsearch.tsx
@@ -0,0 +1,80 @@
+import { gall, PrismaClient, species } from '@prisma/client';
+import { GetServerSideProps } from 'next';
+import Link from 'next/link';
+import { ParsedUrlQuery } from 'querystring';
+import { Card, CardColumns } from 'react-bootstrap';
+import CardTextCollapse from '../components/cardcollapse';
+import { SearchBar, SearchQuery } from '../components/searchbar';
+
+type SpeciesProp = species & {
+    gall: gall[],
+    hosts: species[],
+    host_galls: species[]
+}
+
+type Props = {
+    species: SpeciesProp[],
+    query: SearchQuery
+};
+
+const speciesLink = (species: SpeciesProp) => {
+    if (species.taxoncode === 'gall') {
+        return <Link href={"gall/[id]"} as={`gall/${species.id}`}><a>{species.name}</a></Link>
+    } else {
+        return <Link href={"host/[id]"} as={`host/${species.id}`}><a>{species.name}</a></Link>
+    }
+
+}
+const GlobalSearch = ({ species, query }: Props): JSX.Element => {
+    return (
+        <div>
+            <CardColumns className='m-2 p-2'>
+                {species.map( species =>
+                    <Card key={species.id} className="shadow-sm">
+                        <Card.Img variant="top" width="200px" src="/images/gall.jpg" />
+                        <Card.Body>
+                            <Card.Title>
+                                { speciesLink(species) }
+                            </Card.Title>
+                            <CardTextCollapse text={species.description} />
+                        </Card.Body>
+                    </Card>
+                )}
+            </CardColumns>
+            <SearchBar query={ {...query} }></SearchBar>
+        </div>
+    )
+}
+
+export const getServerSideProps: GetServerSideProps = async (context: { query: ParsedUrlQuery; }) => {
+    // add wildcards to search phrase
+    const q = `%${context.query.searchText as string}%`;
+    // Useful for logging SQL that is genereated for debugging the search
+    // const newdb = new PrismaClient({log: ['query']}); 
+    const newdb = new PrismaClient();
+
+    const species = await newdb.species.findMany({
+        include: {
+            host_galls: true,
+            hosts: true,
+            gall: true,
+        },
+        where: {
+            OR: [
+                { name: { contains: q} },
+                { description: { contains: q } },
+                { commonnames: { contains: q } },
+                { synonyms: { contains: q } },
+            ]
+        }
+    });
+
+    return {
+        props: {
+            species: species,
+            query: q,
+        }
+    }
+}
+
+export default GlobalSearch;
