4ec32c9db5507c700cb25d8df2c82e6b0525f0e8
jeffdc <jeff@nothoo.com>
2021-06-21 13:20:58 -0400

    Fixed type issue with Section creation. Also committing migrated database for testing.

--- libs/db/species.ts
+++ libs/db/species.ts
@@ -106,51 +106,53 @@ export const speciesUpdateData = (sp: SpeciesUpsertFields) => ({
 });
 
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
-export const speciesCreateData = (sp: SpeciesUpsertFields) => ({
-    abundance: connectIfNotNull<Prisma.abundanceCreateNestedOneWithoutSpeciesInput, string>('abundance', sp.abundance),
-    datacomplete: sp.datacomplete,
-    name: sp.name,
-    aliasspecies: {
-        create: sp.aliases.map((a) => ({
-            alias: { create: { description: a.description, name: a.name, type: a.type } },
-        })),
-    },
-    speciestaxonomy: {
-        // the speciestaxonomy records will be new since the gall is new
-        // the genus and related
-        create: [
-            {
-                taxonomy: {
-                    // the genus might be new
-                    connectOrCreate: {
-                        where: { id: sp.fgs.genus.id },
-                        create: {
-                            description: sp.fgs.genus.description,
-                            name: sp.fgs.genus.name,
-                            type: GENUS,
-                            parent: {
-                                connect: {
-                                    id: sp.fgs.family.id,
-                                },
-                            },
-                            children: {
-                                create: { taxonomy_id: sp.fgs.family.id },
-                            },
+export const speciesCreateData = (sp: SpeciesUpsertFields) => {
+    const speciesTaxonomyCreates = new Array<Prisma.speciestaxonomyCreateWithoutSpeciesInput>({
+        taxonomy: {
+            // the genus might be new
+            connectOrCreate: {
+                where: { id: sp.fgs.genus.id },
+                create: {
+                    description: sp.fgs.genus.description,
+                    name: sp.fgs.genus.name,
+                    type: GENUS,
+                    parent: {
+                        connect: {
+                            id: sp.fgs.family.id,
                         },
+                    },
+                    children: {
+                        create: { taxonomy_id: sp.fgs.family.id },
                     },
                 },
             },
-            // handle section for hosts
-            pipe(
-                sp.fgs.section,
-                O.map((s) => ({ taxonomy: { connect: { id: s.id } } })),
-                O.getOrElseW(constant(null)),
-            ),
-        ].filter((i) => i),
-        // This filter is a hack to remove a possibly null section connect. Prisma craps out otherwise and there is no way
-        // that I can find to optionally connect records.
-    },
-});
+        },
+    });
+
+    // handle section for hosts
+    // This is a hack to deal with the fact that a Secion may not be presen. Prisma craps out if you pass an empty element
+    // and I can find no way to optionally connect records.
+    pipe(
+        sp.fgs.section,
+        O.map((s) => speciesTaxonomyCreates.push({ taxonomy: { connect: { id: s.id } } })),
+    );
+
+    return {
+        abundance: connectIfNotNull<Prisma.abundanceCreateNestedOneWithoutSpeciesInput, string>('abundance', sp.abundance),
+        datacomplete: sp.datacomplete,
+        name: sp.name,
+        aliasspecies: {
+            create: sp.aliases.map((a) => ({
+                alias: { create: { description: a.description, name: a.name, type: a.type } },
+            })),
+        },
+        speciestaxonomy: {
+            // the speciestaxonomy records will be new since the gall is new
+            // the genus and related
+            create: speciesTaxonomyCreates,
+        },
+    };
+};
 
 export const speciesTaxonomyAdditionalUpdateSteps = (sp: SpeciesUpsertFields): PrismaPromise<unknown>[] => [
     // the genus could have been changed and might be new
