d1395c762884c7154887280b990b6bfa320b53f5
jeffdc <jeff@nothoo.com>
2021-11-24 13:06:24 -0500

    Disabled DB debug logging and added new help text to rename dialog.

--- components/editname.tsx
+++ components/editname.tsx
@@ -43,6 +43,13 @@ const EditName = ({ type, keyField, defaultValue, showModal, setShowModal, renam
                             <br />
                             <input type="checkbox" onChange={(e) => setAddAlias(e.currentTarget.checked)} /> Add Alias for old
                             name?
+                            <div className="mt-2 small">
+                                If you want to reassign the species to a different genus simply enter the new name (full couplet)
+                                with the new genus name. If the genus does not yet exist it will be created and assigned to the
+                                same family. If already exists the species will be re-assigned to the genus. You have be be
+                                careful and make sure that you type the genus name exactly as it is if it already exists,
+                                otherwise a new genus with the differing name will be created.
+                            </div>
                         </>
                     )}
                 </Modal.Body>
